cf017e335b3113bc62a93e363b3386bf49a6443f
Aditya Garg <aditya.garg3198@gmail.com>
2026-05-20 00:03:02 +0530

    add cmdk dep, wire next/font google fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,23 @@
+import { Instrument_Serif, IBM_Plex_Sans, JetBrains_Mono } from 'next/font/google';
+
+export const instrumentSerif = Instrument_Serif({
+  subsets: ['latin'],
+  weight: ['400'],
+  style: ['normal', 'italic'],
+  variable: '--font-instrument-serif',
+  display: 'swap',
+});
+
+export const plexSans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-plex-sans',
+  display: 'swap',
+});
+
+export const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500', '600'],
+  variable: '--font-jetbrains-mono',
+  display: 'swap',
+});
